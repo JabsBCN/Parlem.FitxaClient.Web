f82f0cecfb708b4bb81717bfefba2c970e563b19
Xavier Garcia <javi.vbs@gmail.com>
2022-09-16 17:57:09 +0200

    use client to get customer and products from api

--- src/Parlem.FitxaClient.Web/src/app/app.component.ts
+++ src/Parlem.FitxaClient.Web/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomerClient, CustomerDto } from './customer.api.service';
+import { ProductClient, ProductDto } from './customer.api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Parlem-FitxaClient-Web';
+
+  constructor(private customerClient: CustomerClient, private productClient: ProductClient) { }
+
+  public customer: CustomerDto;
+  public products: ProductDto[];
+
+  ngOnInit(): void {
+    this.customerClient.details(11111)
+      .subscribe(
+        (customer: CustomerDto) => {
+          this.customer = customer;
+        });
+
+    this.productClient.products(11111)
+      .subscribe(
+        (products: ProductDto[]) => {
+          this.products = products;
+        });
+  }
 }
--- src/Parlem.FitxaClient.Web/src/app/app.module.ts
+++ src/Parlem.FitxaClient.Web/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import { ProductClient, API_BASE_URL } from './customer.api.service'; 
+import { MessageClient, API_BASE_URL } from './chat.api.client.service'; 
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http'; 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule 
   ],
-  providers: [],
+  providers: [
+    MessageClient,
+    { provide: API_BASE_URL, useValue: environment.API_BASE_URL }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
